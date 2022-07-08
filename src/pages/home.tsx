cb25dbf08585527f128bfd9355a4df49ec0093e5
zhouxian <726839037@qq.com>
2022-07-08 17:35:05 +0800

    header做完了

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './home.scss';
-
+import headerBg from '../images/head.png'
 export const Home = () => {
   return (
     <div>
-      <div className="x"></div>
+      <div className="x" style={{backgroundImage:`url(${headerBg})`}}></div>
     </div>
   );
 };
