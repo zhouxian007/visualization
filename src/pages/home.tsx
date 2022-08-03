7ef9e757e98c7ffdc5ca0d7fd0eaee442f61f2fa
zhouxian <726839037@qq.com>
2022-08-03 14:29:03 +0800

    网格布局

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,8 +3,16 @@ import './home.scss';
 import headerBg from '../images/head.png'
 export const Home = () => {
   return (
-    <div>
-      <div className="x" style={{backgroundImage:`url(${headerBg})`}}></div>
+
+      <div className="home" >
+      <header style={{backgroundImage:`url(${headerBg})`}}/>
+      <main  className="main">
+          <section className="section1"></section>
+          <section className="section2"></section>
+          <section className="section3"></section>
+          <section className="section4"></section>
+          <section className="section5"></section>
+      </main>
     </div>
   );
 };
